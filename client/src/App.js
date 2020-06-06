6547e1d569d0de1da93082b26820362fc8288718
arun-ajay <arunajay42795@gmail.com>
2020-06-06 18:12:31 -0400

    Created client and server folder. Created 3 main page components for client

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import LandingPage from  './pages/LandingPage/LandingPage'
+
+
+function App() {
+  var hello = "Hi Dev!"
+
+
+  return (
+  <div>
+    <LandingPage></LandingPage>
+  </div>
+  );
+}
+
+export default App;
---
+++ client/src/pages/LandingPage/LandingPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './LandingPage.css';
+
+
+
+function LandingPage(){
+
+
+    return(
+        <div>
+            Edit me!
+        </div>
+    );
+}
+
+export default LandingPage;
